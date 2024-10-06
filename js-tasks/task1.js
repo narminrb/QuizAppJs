e468eade94f0112e5561a4a34b2b134fe676ad6d
narminrb <narminrb@code.edu.az>
2024-10-06 15:54:21 +0400

    done

--- js-tasks/task1.js
+++ js-tasks/task1.js
@@ -1,14 +1,20 @@
-let number = parseInt(prompt("Please enter a number:")); 
-let originalNumber = number;
-let reverse = 0;
+function isPalindrome(number) {
+    let originalNumber = number;
+    let reverse = 0;
 
-if (number < 0) {
-    console.log(false);
-} else {
-    while (number > 0) {
-        let lastDigit = number % 10; 
-        reverse = reverse * 10 + lastDigit; 
-        number = Math.floor(number / 10); 
+    if (number < 0) {
+        return false; 
     }
-    console.log(originalNumber === reverse); 
+
+    while (number > 0) {
+        let lastDigit = number % 10;  
+        reverse = reverse * 10 + lastDigit;  
+        number = Math.floor(number / 10);  
+    }
+
+    return originalNumber === reverse;  
 }
+
+
+let number = parseInt(prompt("Please enter a number:"));
+console.log(isPalindrome(number));
